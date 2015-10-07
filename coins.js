a695f855e40c4470e25a3bc5e1dbdf6a8aa3ed5c
Paul Doyle <pauleamondoyle@gmail.com>
2015-10-07 10:06:15 -0500

    took out map function

--- coins.js
+++ coins.js
@@ -4,24 +4,21 @@ var coins = function(num){
 	var results = []
 	
 		while(num>=25){
-			results.push('25')
+			results.push(25)
 			num -=25
 		}
 		while(num>=10){
-			results.push('10')
+			results.push(10)
 			num-=10
 		}
 		while(num>=5){
-			results.push('5')
+			results.push(5)
 			num -=5
 		}
 		while(num>=1){
-			results.push('1')
+			results.push(1)
 			num -=1
 		}
 
-	var finalResult = results.map(function(el){
-		return parseInt(el)
-	})
-	return finalResult
+	return results
 }
